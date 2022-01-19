93319940f134382d0975a7b17acd3372f4a097a3
Gustavo Igor <gustavo10fac_igor@outlook.com>
2022-01-19 16:30:46 -0300

    PARTE 2 (sem interface)

--- src/model/service/RentalService.java
+++ src/model/service/RentalService.java
@@ -1,5 +1,8 @@
 package model.service;
 
+import model.entities.CarRental;
+import model.entities.Invoice;
+
 public class RentalService {
     
     public Double pricePerHour;
@@ -14,6 +17,22 @@ public class RentalService {
     }
     
     public void processInvoice(CarRental carRental){
+        long t1 = carRental.getStart().getTime();
+        long t2 = carRental.getFinish().getTime();
+        double hours = (double)(t2 - t1) / 1000 / 60 / 60;
+        
+        double basicPayment;
+        if(hours <= 12.0){
+            basicPayment = pricePerHour * Math.ceil(hours);
+        }
+        
+        else {
+            basicPayment = pricePerHour * Math.ceil(hours / 24);
+        }
+        
+        double tax = taxService.tax(basicPayment);
+        
+        carRental.setInvoice(new Invoice(basicPayment, tax));
         
     }
     
